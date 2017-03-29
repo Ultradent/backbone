80776f290d6a048cc41d0840ac5e6ef4e63a41e9
troywatt <troywatt@gmail.com>
2017-03-29 11:29:20 -0600

    removed UMD from backbone lib

--- backbone.js
+++ backbone.js
@@ -12,24 +12,11 @@
   var root = (typeof self == 'object' && self.self === self && self) ||
             (typeof global == 'object' && global.global === global && global);
 
-  // Set up Backbone appropriately for the environment. Start with AMD.
-  if (typeof define === 'function' && define.amd) {
-    define(['underscore', 'jquery', 'exports'], function(_, $, exports) {
-      // Export global even in AMD case in case this script is loaded with
-      // others that may still expect a global Backbone.
-      root.Backbone = factory(root, exports, _, $);
-    });
-
-  // Next for Node.js or CommonJS. jQuery may not be needed as a module.
-  } else if (typeof exports !== 'undefined') {
-    var _ = require('underscore'), $;
-    try { $ = require('jquery'); } catch (e) {}
-    factory(root, exports, _, $);
-
-  // Finally, as a browser global.
-  } else {
-    root.Backbone = factory(root, {}, root._, (root.jQuery || root.Zepto || root.ender || root.$));
-  }
+  // NOTE --> UMD support has been removed from the initialization of this module
+  // this was done due to issues attempting to disable AMD support through
+  // implementing the imports-loader from webpack - this approach did not work as expected
+  // and both jQuery and Underscore were packaged when backbone was imported
+  root.Backbone = factory(root, {}, root._, (root.jQuery || root.Zepto || root.ender || root.$));
 
 })(function(root, Backbone, _, $) {
 
